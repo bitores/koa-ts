0d575694269ad0f255e89587316436561f38fcae
黄政杰 <773155801@qq.com>
2018-06-09 19:51:03 +0800

    a
    a

--- server/config/mail.js
+++
@@ -1,9 +0,0 @@
-// username, sender_address  should be the same address
-
-export default {
-	host: 'smtp.office365.com', // SMTP服务提供商域名
-	port: 587,
-	username: 'huangzhengjie@btb.com', // 用户名/用户邮箱
-	password: '*******', // 邮箱密码
-	sender_address: 'huangzhengjie@btb.com'
-};
